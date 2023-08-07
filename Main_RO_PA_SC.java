da4f4a3d9ec3315f61408cce14b7dedb2fc88fe9
ElJulii <134737429+ElJulii@users.noreply.github.com>
2023-08-07 13:16:06 -0500

    Actulización 1.1
    I have added (try catch for InputMissMatchException in the intro of start of finish the game). In addition I created a second method called "intro" with a recusion method. Moreover, I changed some letters for just the presentation of the game.

--- Main_RO_PA_SC.java
+++ Main_RO_PA_SC.java
@@ -1,18 +1,27 @@
 package games.rock_scissors_paper;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main_RO_PA_SC {
     public static void main(String[] args) {
-
+        //Actualization 1.1
+        intro();
+    }
+    public static void intro() {
         Scanner scanner = new Scanner(System.in);
-        int start_finish = 1;
+        int start_finish = 3;
         while (start_finish > 0) {
             System.out.println("\nWrite 1 to start the game, write 2 to finish it...");
-            start_finish = scanner.nextInt();
+            try {
+                start_finish = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Just write 1 or 2... Try again.");
+                intro();
+            }
             if (start_finish == 1) {
-                start();
+                startGame();
             } else if (start_finish == 2) {
                 break;
             } else {
@@ -21,7 +30,7 @@ public class Main_RO_PA_SC {
         }
         System.out.println("\nTHANKS FOR PLAYING!");
     }
-    public static void start() {
+    public static void startGame() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
@@ -32,7 +41,7 @@ public class Main_RO_PA_SC {
 
         int points_player = 0, points_machine = 0;
 
-        System.out.println("The best of five times wins! good luck!");
+        System.out.println("The best of five times wins! Good luck!");
         System.out.println("Select: ");
         System.out.println("1- paper");
         System.out.println("2- scissors");
@@ -42,7 +51,7 @@ public class Main_RO_PA_SC {
             int your_choose = scanner.nextInt();
             int opponent = random.nextInt(1, 4);
 
-            if (your_choose == opponent) System.out.println("Equals\n");
+            if (your_choose == opponent) System.out.println("Equals.\n");
             else if (your_choose == 1) {
                 paper(opponent);
                 if (opponent == 2) points_machine++;
@@ -56,11 +65,11 @@ public class Main_RO_PA_SC {
                 if (opponent == 1) points_machine++;
                 else points_player++;
             } else {
-                System.out.println("Option is not valid, please try again\n");
+                System.out.println("Option is not valid, please try again.\n");
                 times--;
             }
         }
-        if (points_machine > points_player) System.out.println(namePlayer + ", you have lost... sorry, try again");
+        if (points_machine > points_player) System.out.println(namePlayer + ", you have lost... sorry, try again.");
         else if (points_player > points_machine) System.out.println(namePlayer + " have Won! congratulations!");
         else System.out.println("TIE!");
         System.out.println("Machine= " + points_machine);
@@ -68,31 +77,31 @@ public class Main_RO_PA_SC {
     }
     public static void paper(int machine_Option) {
         if (machine_Option == 2) {
-            System.out.println("You miss a point!");
+            System.out.println("You MISS a point!");
             System.out.println("Machine: SCISSORS\n");
         }
         else {
-            System.out.println("You win a point!");
+            System.out.println("You WIN a point!");
             System.out.println("Machine: ROCK\n");
         }
     }
     public static void scissors(int machine_Option) {
         if (machine_Option == 3) {
-            System.out.println("You miss a point!");
+            System.out.println("You MISS a point!");
             System.out.println("Machine: ROCK\n");
         }
         else {
-            System.out.println("You win a point!");
+            System.out.println("You WIN a point!");
             System.out.println("Machine: PAPER\n");
         }
     }
     public static void rock(int machine_Option) {
         if (machine_Option == 1) {
-            System.out.println("You miss a point!");
+            System.out.println("You MISS a point!");
             System.out.println("Machine: PAPER\n");
         }
         else {
-            System.out.println("You win a point!");
+            System.out.println("You WIN a point!");
             System.out.println("Machine: SCISSORS\n");
         }
     }
